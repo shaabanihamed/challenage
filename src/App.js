a1cdcbf88639bde36f7936ffbccdf62ffab1902f
hamed shaabani <hamedshaabani98@gmail.com>
2023-02-25 11:31:06 +0330

    fix: login context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+
 // routes
 import Router from './routes';
 // theme
@@ -7,17 +9,22 @@ import ThemeProvider from './theme';
 // components
 import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
+import { userContext } from './userContext/userContext';
 
 // ----------------------------------------------------------------------
 
 export default function App() {
+  const defaultValue = JSON.parse(localStorage.getItem('loginState')) || { email: '', password: '' };
+  const [context, setContext] = useState(defaultValue);
   return (
     <HelmetProvider>
       <BrowserRouter>
         <ThemeProvider>
           <ScrollToTop />
           <StyledChart />
-          <Router />
+          <userContext.Provider value={[context, setContext]}>
+            <Router />
+          </userContext.Provider>
         </ThemeProvider>
       </BrowserRouter>
     </HelmetProvider>
